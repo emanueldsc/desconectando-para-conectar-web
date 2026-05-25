8936e1b58660501f9ac098fa914bff87d55867b7
Emanuel Douglas <emanuel.douglas.sc@gmail.com>
2026-05-25 00:52:21 -0300

    Correção enviada para o repositório remoto. O commit inclui as seguintes alterações:

--- src/app/shared/services/api.config.ts
+++ src/app/shared/services/api.config.ts
@@ -1,5 +1,5 @@
 import { InjectionToken } from '@angular/core';
-import { environment } from './environment';
+import { environment } from '../../../environments/environment';
 
 export const API_BASE_URL = new InjectionToken<string>('API_BASE_URL', {
   providedIn: 'root',
